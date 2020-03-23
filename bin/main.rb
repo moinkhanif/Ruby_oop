feb0cb5d1e9c7ff4d3748be8381a9256cbaf9080
Moin <kmoin786@gmail.com>
2020-03-24 03:49:06 +0530

    add basic interface to the game

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,13 @@
 #!/usr/bin/env ruby
+require 'terminal-table'
 
-puts 'Hello World!'
-puts "New branch"
+def interface
+  ttt_rows = []
+  ttt_rows << [1, 2, 3]
+  ttt_rows << [4, 5, 6]
+  ttt_rows << [7, 8, 9]
+  table = Terminal::Table.new rows: ttt_rows
+  puts table
+end
+
+interface
