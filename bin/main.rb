1be3351745ea6a5389e313f5c74f5d300d4c2b8d
Sam <sam@Sams-MacBook-Pro.local>
2020-03-24 19:28:28 +0300

    Tic-Tac-Toe game logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,26 +1,24 @@
 #!/usr/bin/env ruby
+# require 'colorize'  # Need to run 'gem install colorize'
 
-# def getmove
-#   move = gets.chomp
-# end
-puts 'palyer one enter name'
-# player1_name = gets.chomp
-puts 'palyer two enter name'
-# player2_name = gets.chomp
-player1 = Player.new(plaer1_name, 'X')
-player2 = Player.new(plaer2_name, 'O')
-board = Board.new
-game = Game.new(board, player1, player2)
-while gmae.game_end
-  puts 'player make a move'
-  current_move = getmove
-  current_move = getmove while game.valid_move?(current_move)
-  game.store_move(current_move)
-  if game.game_end
-    if game.winner_name
-      puts game.winner_name
-    else
-      puts 'it is a draw'
-    end
+# self customized libary
+require '../lib/interface.rb'
+require '../lib/board.rb'
+require '../lib/player.rb'
+require '../lib/game.rb'
+
+include Interface
+
+loop do
+  get_input('welcome')
+
+  player1 = Player.new('player1', 'X')
+  player2 = Player.new('player2', 'O')
+  board = Board.new
+  game = Game.new(player1, player2, board)
+
+  unless game.play
+    get_input('finish')
+    break
   end
 end
