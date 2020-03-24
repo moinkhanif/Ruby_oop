6cb169046fe54201f20b77787f5cec14750ae99d
Sam <sam@Sams-MacBook-Pro.local>
2020-03-24 19:46:58 +0300

    Adding lib files

---
+++ lib/player.rb
@@ -0,0 +1,16 @@
+class Player
+  include Interface
+
+  attr_reader :char
+  attr_accessor :name, :inputs
+
+  def initialize(name, char)
+    @name = get_input('name', name)
+    @inputs = []
+    @char = char
+  end
+
+  def move(input)
+    @inputs << input
+  end
+end
